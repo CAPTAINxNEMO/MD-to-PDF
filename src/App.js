21403de9b46c2cc5d4adeae52c819a9ddcb259f8
Shashank Bhave <127836260+CAPTAINxNEMO@users.noreply.github.com>
2024-04-19 21:21:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Markdown from './components/markdown';
+
+function App() {
+	return (
+		<div className='App w-screen h-screen bg-primary '>
+			<Markdown/>
+		</div>
+		
+	);
+}
+
+export default App;
---
+++ src/components/markdown.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './markdown.css';
+import "highlight.js/styles/github-dark.css";
+import hljs from "highlight.js";
+import { marked } from "marked";
+// import { mdToPdf } from "md-to-pdf";
+const renderer = new marked.Renderer();
+const MathJax = window.MathJax;
+
+renderer.code = function (code, language) {
+    const highlighted = hljs.highlightAuto(code).value;
+    return `<pre><code class="hljs ${language} ">${highlighted}</code></pre>`;
+};
+
+marked.setOptions({ renderer });
+const Markdown = () => {
+    const [text, setText] = useState("");
+    const [markdown, setMarkdown] = useState("");
+    const htmlPreviewRef = useRef(null);
+
+    const handleChange = (event) => {
+    setText(event.target.value);
+    setMarkdown(marked(event.target.value));
+    };
+
+    useEffect(() => {
+        if (htmlPreviewRef.current) {
+        const tempDiv = document.createElement("div");
+        tempDiv.innerHTML = markdown;
+        MathJax.typesetPromise([tempDiv]).then(() => {
+            htmlPreviewRef.current.innerHTML = tempDiv.innerHTML;
+        }).catch((err) => {
+            console.error(err.message);
+        });
+        }
+    }, [markdown]);
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Tab") {
+        event.preventDefault();
+        const start = event.target.selectionStart;
+        const end = event.target.selectionEnd;
+        const value = event.target.value;
+        setText(value.substring(0, start) + " ".repeat(4) + value.substring(end));
+        event.target.selectionStart = event.target.selectionEnd = start + 4;
+        }
+    };
+
+    // const convertToPdf = async () => {
+    //   try {
+    //     const pdf = await mdToPdf({ content: markdown });
+    //     fs.writeFileSync("Output.pdf", pdf.content);
+    //   } catch (err) {
+    //     console.error(err);
+    //   }
+    // }
+
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+            <div className="edit-assignment-container rounded-md text-white bg-secondary">
+                <textarea className="markdown-editor bg-secondary p-3" placeholder="Edit" autoFocus value={text} onChange={handleChange} onKeyDown={handleKeyDown}></textarea>
+                <div ref={htmlPreviewRef} className="html-preview bg-white text-black" dangerouslySetInnerHTML={{ __html: markdown }}></div>
+            </div>
+            <div className="button-container md-4">
+                <button /*onClick={convertToPdf}*/ className="bg-secondary text-white hover:text-primary px-10 py-4 rounded-md transform translate-y-[-30px]">Convert to PDF</button>
+            </div>
+        </div>
+    )
+}
+
+export default Markdown
